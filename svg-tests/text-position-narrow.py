37005f21601b7d16931cd76931d20d4b7acc7da2
johnandrea <46268748+johnandrea@users.noreply.github.com>
2025-10-01 12:44:14 -0300

    compute text path

--- svg-tests/text-position-narrow.py
+++ svg-tests/text-position-narrow.py
@@ -1,7 +1,8 @@
 #import sys
 import math
 
-page_size = 500
+page_x = 500
+page_y = 1000
 
 def roundstr( x ):
     # output of 2 digits ought to be enough
@@ -9,10 +10,9 @@ def roundstr( x ):
 
 
 def output_header():
-    size = str( page_size )
     print( '<?xml version="1.0" standalone="no"?>' )
-    print( '<svg width="' + size + 'pt" height="' + size + 'pt"' )
-    print( ' viewBox="0.00 0.00 ' + size + '.00 ' + size + '.00"' )
+    print( '<svg width="' + str(page_x) + 'pt" height="' + str(page_y) + 'pt"' )
+    print( ' viewBox="0.00 0.00 ' + str(page_x) + '.00 ' + str(page_y) + '.00"' )
     print( ' version="1.1"' )
     print( ' xmlns="http://www.w3.org/2000/svg"' )
     print( ' xmlns:xlink="http://www.w3.org/1999/xlink">' )
@@ -36,28 +36,47 @@ def output_text( x, y, size, s ):
     print( '<text font-size="' + roundstr(size) + '" font-family="Times New Roman,serif"' )
     print( ' x="' + roundstr(x) + '" y="' + roundstr(y) + '">' + s + '</text>' )
 
+def setup_circles( x, y ):
+    center = roundstr(x) + ',' + roundstr(y)
+
+    # line along the x axis
+    print( '<path d="M' + center + ' ' + roundstr(cx+page_x/2) + ',' + roundstr(y) + '" style="stroke:blue;" />' )
+    # and a dot at the center
+    red_dot( x, y )
+
+    # show circles at both those radii
+    print( '<circle cx="' + roundstr(x) + '" cy="' + roundstr(y) + '"' )
+    print( ' fill="none" stroke="black" r="' + roundstr(inner) + '" />' )
+    print( '<circle cx="' + roundstr(x) + '" cy="' + roundstr(y) + '"' )
+    print( ' fill="none" stroke="black" r="' + roundstr(outer) + '" />' )
+
+    # remind me of the directions
+    output_text( x - 10, y - 2, 10, 'x &#8594;' )
+    output_text( x - 10, y + 11, 10, 'y &#8595;' )
+
+    # relative to center
+    print( '<g transform="translate(' + center + ')">' )
+
+def finish_circles():
+    # undo the transform
+    print( '</g>' )
+
+def draw_sector():
+    print( '<path style="stroke:grey; fill:lightgreen;"' )
+    print( 'd="M' + p1 )
+    r = roundstr(inner) + ',' + roundstr(inner)
+    print( 'A' + r + ' 0 0 1 ' + p2 )
+    print( 'L' + p3 )
+    r = roundstr(outer) + ',' + roundstr(outer)
+    print( 'A' + r + ' 0 0 0 ' + p4 )
+    print( 'z" />' )
+
 output_header()
 
-# instructions
-x = 8
-y = 15
-f = 13
-output_text( x, y, f, 'narrow slices require sideway text' )
-y += f + 2
-output_text( x, y, f, 'from P7 to P8' )
-y += f + 2
-output_text( x, y, f, 'P7: x=inner, y=0' )
-y += f + 2
-output_text( x, y, f, 'P8: x=outer, y=0' )
-y += f + 2
-output_text( x, y, f, 'width = outer - inner' )
-y += f + 2
-output_text( x, y, f, 'middle N: x=inner + (outer-inner)/2, y=0' )
-
-y = 260
-output_text( x, y, f, 'second example' )
-y += f + 2
-output_text( x, y, f, 'text along the edge of the slice' )
+# placement of first example
+cx = 340
+cy = 40
+cy_inc = 295 - 40
 
 # radius
 inner = 80
@@ -67,145 +86,79 @@ outer = 150
 d = 15
 half_d = math.radians( d/2.0 )
 
-# placement of first example
-cx = 300
-cy = 40
-center = roundstr(cx) + ',' + roundstr(cy)
+# instructions
+x = 8
+text_y = 15
+y = text_y
+f = 13
+output_text( x, y, f, 'Narrow slices require sideway text from P7 to P8:' )
+y += f + 2
+output_text( x, y, f, 'new-d = factor * d/2' )
+y += f + 2
+output_text( x, y, f, 'slice-width = outer - inner' )
+y += f + 2
+output_text( x, y, f, 'length-margin = ( 1 - factor ) * slice-width' )
+y += f + 2
+output_text( x, y, f, 'new-inner = inner + length-margin' )
+y += f + 2
+output_text( x, y, f, 'new-outer = outer - length-margin' )
+y += f + 2
+output_text( x, y, f, 'P7: x=new-inner*cos(new-d), y=new-inner*sin(new-d)' )
+y += f + 2
+output_text( x, y, f, 'P8: x=new-outer*cos(new-d), y=new-outer*sin(new-d)' )
 
-# line along the x axis
-print( '<path d="M' + center + ' ' + roundstr(cx+page_size/2) + ',' + roundstr(cy) + '" style="stroke:blue;" />' )
-# and a dot at the center
-red_dot( cx, cy )
+text_y += cy_inc
+y = text_y
+output_text( x, y, f, 'Text area:' )
+y += f + 2
+output_text( x, y, f, 'middle of the text line at M' )
+y += f + 2
+output_text( x, y, f, 'middle = new-inner + ( new-outer - new_inner )/2' )
+y += f + 2
+output_text( x, y, f, 'M: x=middle*cos(new-d), y=middle*sin(new-d)' )
+y += f + 2
+y += f + 2
+output_text( x, y, f, 'height of text area at P9' )
+y += f + 2
+output_text( x, y, f, 'P9: x=P7[x], y=-P7[y]' )
+y += f + 2
+output_text( x, y, f, 'height = 2 * P7[y]' )
 
-# show circles at both those radii
-print( '<circle cx="' + roundstr(cx) + '" cy="' + roundstr(cy) + '"' )
-print( ' fill="none" stroke="black" r="' + roundstr(inner) + '" />' )
-print( '<circle cx="' + roundstr(cx) + '" cy="' + roundstr(cy) + '"' )
-print( ' fill="none" stroke="black" r="' + roundstr(outer) + '" />' )
+text_y += cy_inc
+y = text_y
+output_text( x, y, f, 'Place the text:' )
+y += f + 2
+output_text( x, y, f, 'path from p7 to p8' )
+y += f + 2
+output_text( x, y, f, 'place text along that path' )
 
-# remind me of the directions
-output_text( cx - 10, cy - 2, 10, 'x &#8594;' )
-output_text( cx - 10, cy + 11, 10, 'y &#8595;' )
+text_y += cy_inc
+y = text_y
+output_text( x, y, f, 'Show the section rotated:' )
 
-# relative to center
-print( '<g transform="translate(' + center + ')">' )
-
-# reverse y because in svg y increases downward
-p1_x = inner * math.cos(half_d)
-p1_y = - inner * math.sin(half_d)
-p1 = roundstr(p1_x) + ',' + roundstr(p1_y)
-# put a dot and text there
-blue_dot( p1_x, p1_y )
-#output_text( p1_x, p1_y - 5, 10, 'P1' )
-
-p2_x = p1_x
-p2_y = - p1_y
-p2 = roundstr(p2_x) + ',' + roundstr(p2_y)
-blue_dot( p2_x, p2_y )
-#output_text( p2_x, p2_y + 12, 10, 'P2' )
-
-p3_x = outer * math.cos(half_d)
-p3_y = outer * math.sin(half_d)
-p3 = roundstr(p3_x) + ',' + roundstr(p3_y)
-blue_dot( p3_x, p3_y )
-#output_text( p3_x + 5, p3_y, 10, 'P3' )
-
-p4_x = p3_x
-p4_y = - p3_y
-p4 = roundstr(p4_x) + ',' + roundstr(p4_y)
-blue_dot( p4_x, p4_y )
-#output_text( p4_x + 4, p4_y, 10, 'P4' )
-
-# begin the sector
-print( '<path style="stroke:grey; fill:lightgreen;"' )
-print( 'd="M' + p1 )
-r = roundstr(inner) + ',' + roundstr(inner)
-print( 'A' + r + ' 0 0 1 ' + p2 )
-print( 'L' + p3 )
-r = roundstr(outer) + ',' + roundstr(outer)
-print( 'A' + r + ' 0 0 0 ' + p4 )
-print( 'z" />' )
-
-p7_x = inner
-p7_y = 0
-blue_dot( p7_x, p7_y )
-output_text( p7_x + 4, p7_y - 2, 10, 'P7' )
-p7 = roundstr(p7_x) + ',' + roundstr(p7_y)
-
-p8_x = outer
-p8_y = - 0
-blue_dot( p8_x, p8_y )
-output_text( p8_x + 4, p8_y - 2, 10, 'P8' )
-p8 = roundstr(p8_x) + ',' + roundstr(p8_y)
-
-n_x = inner + (outer - inner)/2
-n_y = - 0
-blue_dot( n_x, n_y )
-output_text( n_x, n_y - 4, 8, 'N' )
-
-# connect with a line
-print( '<path d="M' + p7 + ' ' + p8 + '" style="stroke:red;" />' )
-
-print( '</g>' )
-
-# second example
-cx = 300
-cy = 380
-center = roundstr(cx) + ',' + roundstr(cy)
-
-# line along the x axis
-print( '<path d="M' + center + ' ' + roundstr(cx+page_size/2) + ',' + roundstr(cy) + '" style="stroke:blue;" />' )
-# and a dot at the center
-red_dot( cx, cy )
-
-# show circles at both those radii
-print( '<circle cx="' + roundstr(cx) + '" cy="' + roundstr(cy) + '"' )
-print( ' fill="none" stroke="black" r="' + roundstr(inner) + '" />' )
-print( '<circle cx="' + roundstr(cx) + '" cy="' + roundstr(cy) + '"' )
-print( ' fill="none" stroke="black" r="' + roundstr(outer) + '" />' )
-
-# remind me of the directions
-output_text( cx - 10, cy - 2, 10, 'x &#8594;' )
-output_text( cx - 10, cy + 11, 10, 'y &#8595;' )
-
-# relative to center
-print( '<g transform="translate(' + center + ')">' )
+# first example
+setup_circles( cx, cy )
 
 # reverse y because in svg y increases downward
 p1_x = inner * math.cos(half_d)
 p1_y = - inner * math.sin(half_d)
 p1 = roundstr(p1_x) + ',' + roundstr(p1_y)
-# put a dot and text there
-blue_dot( p1_x, p1_y )
-#output_text( p1_x, p1_y - 5, 10, 'P1' )
 
 p2_x = p1_x
 p2_y = - p1_y
 p2 = roundstr(p2_x) + ',' + roundstr(p2_y)
-blue_dot( p2_x, p2_y )
-#output_text( p2_x, p2_y + 12, 10, 'P2' )
 
 p3_x = outer * math.cos(half_d)
 p3_y = outer * math.sin(half_d)
 p3 = roundstr(p3_x) + ',' + roundstr(p3_y)
-blue_dot( p3_x, p3_y )
-#output_text( p3_x + 5, p3_y, 10, 'P3' )
 
 p4_x = p3_x
 p4_y = - p3_y
 p4 = roundstr(p4_x) + ',' + roundstr(p4_y)
-blue_dot( p4_x, p4_y )
-#output_text( p4_x + 4, p4_y, 10, 'P4' )
 
-# begin the sector
-print( '<path style="stroke:grey; fill:lightgreen;"' )
-print( 'd="M' + p1 )
-r = roundstr(inner) + ',' + roundstr(inner)
-print( 'A' + r + ' 0 0 1 ' + p2 )
-print( 'L' + p3 )
-r = roundstr(outer) + ',' + roundstr(outer)
-print( 'A' + r + ' 0 0 0 ' + p4 )
-print( 'z" />' )
+draw_sector()
+
+# draw the line where the text would go
 
 margin_d_factor = 0.8
 margin_w_factor = 0.95
@@ -216,7 +169,6 @@ slice_width = inner + ( outer - inner )/2
 slice_margin = ( 1.0 - margin_w_factor ) * slice_width
 margined_inner = inner + slice_margin
 margined_outer = outer - slice_margin
-margined_slice_width = margined_inner + ( margined_outer - margined_inner )/2
 
 p7_x = margined_inner * math.cos( margined_d )
 p7_y = margined_inner * math.sin( margined_d )
@@ -230,14 +182,65 @@ blue_dot( p8_x, p8_y )
 output_text( p8_x + 4, p8_y - 2, 10, 'P8' )
 p8 = roundstr(p8_x) + ',' + roundstr(p8_y)
 
-n_x = margined_slice_width * math.cos( margined_d )
-n_y = margined_slice_width * math.sin( margined_d )
-blue_dot( n_x, n_y )
-output_text( n_x, n_y - 4, 8, 'N' )
+# connect with a line
+print( '<path d="M' + p7 + ' ' + p8 + '" style="stroke:red;" />' )
+
+finish_circles()
+
+# second example
+cy += cy_inc
+setup_circles( cx, cy )
+center = roundstr(cx) + ',' + roundstr(cy)
+
+draw_sector()
+
+blue_dot( p7_x, p7_y )
+output_text( p7_x + 4, p7_y - 2, 10, 'P7' )
+
+blue_dot( p8_x, p8_y )
+output_text( p8_x + 4, p8_y - 2, 10, 'P8' )
 
 # connect with a line
 print( '<path d="M' + p7 + ' ' + p8 + '" style="stroke:red;" />' )
 
+middle = margined_inner + ( margined_outer - margined_inner ) / 2
+
+m_x = middle * math.cos( margined_d )
+m_y = middle * math.sin( margined_d )
+blue_dot( m_x, m_y )
+output_text( m_x, m_y - 4, 8, 'M' )
+
+p9_x = p7_x
+p9_y = 0 - p7_y
+blue_dot( p9_x, p9_y )
+output_text( p9_x, p9_y - 4, 8, 'P9' )
+p9 = roundstr(p9_x) +','+ roundstr(p9_y)
+# connect with a line
+print( '<path d="M' + p7 + ' ' + p9 + '" style="stroke:red;" />' )
+
+finish_circles()
+
+# third example
+cy += cy_inc
+setup_circles( cx, cy )
+draw_sector()
+
+print( '<path id="text" d="M' + p7 + ' L' + p8 + '" style="fill:none;" />' )
+print( '<text font_size="12" font-family="Times New Roman,serif">' )
+print( ' <textPath xlink:href="#text" startOffset="5%">Words</textPath>' )
+print( '</text>' )
+
+finish_circles()
+
+# fourth example
+cy += cy_inc
+setup_circles( cx, cy )
+print( '<g transform="rotate(-35)">' )
+draw_sector()
+print( '<text font_size="12" font-family="Times New Roman,serif">' )
+print( ' <textPath xlink:href="#text" startOffset="5%">Words</textPath>' )
+print( '</text>' )
 print( '</g>' )
+finish_circles()
 
 output_trailer()
